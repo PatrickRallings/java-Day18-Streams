aa478fe72b1f99130e89400bb882506afe89ff65
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 14:20:09 -0400

    Instantiated Divisible in main method (for usability)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,25 @@ public class Main {
 //        //Average of Selected Numbers Assignment
 //        AverageOfSelectedNumbers avgS = new AverageOfSelectedNumbers();
 //        avgS.main();
-
-        //Positive Numbers Assignment
-        ArrayList<Integer> nums = new ArrayList();
-        nums.add(3);
-        nums.add(-1);
-        nums.add(1);
-        nums.add(30);
-        nums.add(-16);
-        PositiveNumbers pos = new PositiveNumbers();
-        pos.main(nums);
+//
+//        //Positive Numbers Assignment
+//        ArrayList<Integer> nums = new ArrayList();
+//        nums.add(3);
+//        nums.add(-1);
+//        nums.add(1);
+//        nums.add(30);
+//        nums.add(-16);
+//        PositiveNumbers pos = new PositiveNumbers();
+//        pos.main(nums);
+//
+//        //Divisible Assignment
+//        ArrayList<Integer> numList = new ArrayList();
+//        numList.add(3);
+//        numList.add(-15);
+//        numList.add(1);
+//        numList.add(23);
+//        numList.add(-18);
+//        Divisible div = new Divisible();
+//        div.main(numList);
     }
 }
