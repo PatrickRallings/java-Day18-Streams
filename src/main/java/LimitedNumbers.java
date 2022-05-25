15fe34a656884655fcdfccf1b00e5ed054692c94
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 14:41:53 -0400

    Created class (this) for taking in user-input numbers (until negative) and outputting all numbers input between (and including) 1 and 5

---
+++ src/main/java/LimitedNumbers.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LimitedNumbers {
+
+    public void main(){
+        System.out.println("Enter as many positive numbers as you would like and enter a negative number to stop.");
+        List<Double> inputList = new ArrayList<>();
+        while (true) {
+            Double input = scanner();
+            if (input < 0) {
+                break;
+            }
+            inputList.add(input);
+        }
+        inputList.stream().filter(n -> n >=1 && n <= 5).forEach(System.out::println);
+    }
+
+    private Double scanner(){
+        Scanner scanner = new Scanner(System.in);
+        return Double.valueOf(scanner.nextLine());
+    }
+}
