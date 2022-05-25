d7685ed21e168a690a7212abff6d7a2b8e9fde3b
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 16:21:27 -0400

    Class for instantiating people to add and sort with the (future) implementation of a UniqueLastNames class

---
+++ src/main/java/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    String firstName;
+    String lastName;
+    int birthYear;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}
