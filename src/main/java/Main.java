6c0923a492973f474f2b92f0c205a07f4eb826be
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 14:14:44 -0400

    Instantiated PositiveNumbers in main method (for usability)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        //Average of Numbers Assignment
+//        //Average of Numbers Assignment
 //        AverageOfNumbers avg = new AverageOfNumbers();
 //        avg.main();
+//
+//        //Average of Selected Numbers Assignment
+//        AverageOfSelectedNumbers avgS = new AverageOfSelectedNumbers();
+//        avgS.main();
 
-        //Average of Selected Numbers Assignment
-        AverageOfSelectedNumbers avgS = new AverageOfSelectedNumbers();
-        avgS.main();
+        //Positive Numbers Assignment
+        ArrayList<Integer> nums = new ArrayList();
+        nums.add(3);
+        nums.add(-1);
+        nums.add(1);
+        nums.add(30);
+        nums.add(-16);
+        PositiveNumbers pos = new PositiveNumbers();
+        pos.main(nums);
     }
 }
