d80f1c50682d19c24a110b1c215ae36fae29aed2
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 17:40:15 -0400

    Added class (this) that parses through a given file's text and subsequently prints out each line

---
+++ src/main/java/ReadingFilesPerLine.java
@@ -0,0 +1,26 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadingFilesPerLine {
+
+    List<String> fileLines = new ArrayList<>();
+
+    public void main(String fileName){
+        read(fileName);
+        print();
+    }
+
+    private void read(String file) {
+        try {
+            Files.lines(Paths.get(file)).forEach(row -> fileLines.add(row));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void print(){
+        fileLines.forEach(System.out::println);
+    }
+}
