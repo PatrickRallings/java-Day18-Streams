b8fe2383506b55baa7746f7a6d8f6e330fa5d49e
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 16:45:54 -0400

    Improved class by adding overloaded constructors (to minimize unrelated responsibilities within other classes)

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,29 +1,51 @@
+import java.util.Scanner;
+
 public class Person {
     String firstName;
     String lastName;
     int birthYear;
 
+    public Person(){
+        setFirstName();
+        setLastName();
+        setBirthYear();
+    }
+
+    public Person(String s){
+        this.firstName = s;
+        setLastName();
+        setBirthYear();
+    }
+
+    private String scanner(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName() {
+        System.out.println("Input first name:");
+        this.firstName = scanner();
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName() {
+        System.out.println("Input last name:");
+        this.lastName = scanner();
     }
 
     public int getBirthYear() {
         return birthYear;
     }
 
-    public void setBirthYear(int birthYear) {
-        this.birthYear = birthYear;
+    public void setBirthYear() {
+        System.out.println("Input birth year:");
+        this.birthYear = Integer.parseInt(scanner());
     }
 }
