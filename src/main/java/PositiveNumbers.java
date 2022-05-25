e72bd3e70e865fe7bd89600f587f38cf2a8a2dd4
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-05-25 14:13:06 -0400

    Created class specifically designed for returning the positive integers within an array (using stream())

---
+++ src/main/java/PositiveNumbers.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PositiveNumbers {
+
+    public void main(ArrayList<Integer> nums){
+        System.out.println(positiveNumbers(nums));
+    }
+
+    private List<Integer> positiveNumbers(ArrayList<Integer> nums){
+        return nums.stream().filter(i -> i > 0).collect(Collectors.toList());
+    }
+}
